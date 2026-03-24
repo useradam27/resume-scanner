40c6409f3c3941ab04b257058d27127d952b8700
useradam27 <50191607+useradam27@users.noreply.github.com>
2026-03-23 20:17:48 -0400

    Backend setup

---
+++ backend/src/main/java/com/adamscanner/resumescanner/ResumescannerApplication.java
@@ -0,0 +1,13 @@
+package com.adamscanner.resumescanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumescannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumescannerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/adamscanner/resumescanner/ResumescannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adamscanner.resumescanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumescannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
