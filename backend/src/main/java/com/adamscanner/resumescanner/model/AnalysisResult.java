68045bac870a3221a7ed8c32d9ce12914f895918
useradam27 <a.gerena1995@gmail.com>
2026-04-18 16:52:58 -0400

    Structure for Resume analysis responce.

---
+++ backend/src/main/java/com/adamscanner/resumescanner/model/AnalysisResult.java
@@ -0,0 +1,24 @@
+package com.adamscanner.resumescanner.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true) //ignore any extra fields in the JSON response
+public class AnalysisResult {
+    private int overallScore; // 1-100 score
+    private List<String> strengths;
+    private List<String> missingKeywords;
+    private List<String> suggestions;
+    private String experienceMatch;
+    private String skillsMatch;
+}
